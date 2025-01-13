cfd8d3ab01adf199b41b9dcf8307e3f17e8566b8
Ricardo Patino <rpatino12@outlook.com>
2025-01-13 10:20:29 -0500

    First commit, add initial setup for a REST API with hapi

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import Hapi from '@hapi/hapi';
+
+const start = async () => {
+    const server = Hapi.server({
+        port: 8000,
+        host: 'localhost',
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: (req, h) => {
+            return h.response('Hello!').code(200);
+        }
+    });
+
+    await server.start();
+    console.log(`Server is listening on ${server.info.uri}`);
+}
+
+process.on('unhandledRejection', err => {
+    console.error(err);
+    process.exit(1);
+});
+
+start();
