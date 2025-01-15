9de95354cae059368d7a02b350f6336b19ea0a56
Ricardo Patino <rpatino12@outlook.com>
2025-01-15 17:03:50 -0500

    Implement Firebase admin and set up Firebase to hapi backend

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ import Hapi from '@hapi/hapi';
 import routes from './routes';
 import { db } from './database';
 import * as admin from 'firebase-admin';
+import credentials from '../credentials.json'
 
-admin.initializeApp();
+admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+});
 
 let server;
 
